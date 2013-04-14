74b7d553bc0ac59ccaabad3e8bfaf7fc16dd25e2
BlackEdder <edder@tkwsping.nl>
2013-04-14 16:37:08 +0100

    Added first test for stream in

---
+++ tests/test_stream_in.hh
@@ -0,0 +1,28 @@
+
+#include <cxxtest/TestSuite.h>
+#include "bson/bson_stream.hh"
+using namespace mongo;
+
+class test {
+	public:
+		double a;
+		double b;
+		test() {};
+		test( double a, double b ) : a(a), b(b) {}
+
+		friend void operator>>( const BSONElement &bel, test &t ) {
+			bel["a"] >> t.a;
+			bel["b"] >> t.b;
+		}
+
+};
+
+class TestIn : public CxxTest::TestSuite {
+	public:
+		void testDouble() {
+			BSONObjBuilder bbuild;
+			bbuild << "a" << 1.0;
+			BSONObj bobj = BSONObjBuilder().append("a", 1.0).obj();
+			TS_ASSERT_EQUALS( bobj, bbuild.obj() );
+		}
+};
